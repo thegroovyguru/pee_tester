8e0f55975c1002711dc326f2b65907cfd3501e81
thegroovyguru <jasonsorensen@hotmail.com>
2014-02-21 16:46:50 -0600

    Update convert_to_grey.c

--- convert_to_grey.c
+++ convert_to_grey.c
@@ -1,50 +1,30 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <stdarg.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
+public static Bitmap MakeGrayscale(Bitmap original)
+{
+   //make an empty bitmap the same size as original
+   Bitmap newBitmap = new Bitmap(original.Width, original.Height);
 
-int line_detection(void){
+   for (int i = 0; i < original.Width; i++)
+   {
+      for (int j = 0; j < original.Height; j++)
+      {
+         //get the pixel from the original image
+         Color originalColor = original.GetPixel(i, j);
 
+         //create the grayscale version of the pixel
+         int grayScale = (int)((originalColor.R * .3) + (originalColor.G * .59)
+             + (originalColor.B * .11));
 
-//grey_image=loadImage("newBitmap.png");
-FILE *infile;
-int num_rows=768;
-int pixels_per_row=1024;
-int column_num;
-int current_pixel_value;
-int dark_pixel_counter[pixels_per_row];
-int temp;
-int current_highest[2];
-int most_dark_line[2];
-int line_location[2];
-int previous_value=0;
-int line_location_matrix[height][3];
+         //create the color object
+         Color newColor =  Color.FromArgb(grayScale, grayScale, grayScale);
 
-infile=fopen(newBitmap.png,"r");
-for (n=0;num_rows;n++)
-	for (column_num=0: pixels_per_row;column_num++)
-		current_pixel_value=getpixel(column_num,n);
-		if (current_pixel_value>previous_value)
-			temp=1;
-		else
-			temp=0;
-		if((current_pixel_value<previous_value)&&temp==1)
-			dark_pixel_counter[column_num]=dark_pixel_counter[column_num]+1;
-		else
-			dark_pixel_counter[column_num]=dark_pixel_counter[column_num];
-	
-		for (int i=0;2;i++)
-			if (dark_pixel_counter[column_num]>current_highest[i])
-				current_highest[i]=dark_pixel_counter[column_num];
-			else
-				current_highest[i]=current_highest[i];
-				most_dark_line[i]=column_num;
-			line_location[i]=most_dark_line[i];
+         //set the new image's pixel to the grayscale version
+         newBitmap.SetPixel(i, j, newColor);
+        }
+    }
 
-for (int q=0;2;q++)
-	for (int j=0;num_rows;j++)
-		line_location_matrix[j][q]=line_location[q]
-
-
-return line_location_matrix;
+    return newBitmap;
 }
